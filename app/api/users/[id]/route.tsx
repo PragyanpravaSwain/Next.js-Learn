a0db360a3a995fff4a72418b2f27d89ebb0db7b3
Pragyan Prava Swin <pragyanpravaswain@gmail.com>
2024-05-06 07:58:01 +0530

    delete data

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -17,7 +17,7 @@ export async function GET(request: NextRequest, {params}: {params: {id: string}}
     return NextResponse.json(user);
 }
 
-export async function PUT(request: NextRequest, {params}: {params: {id: number}}) {
+export async function PUT(request: NextRequest, {params}: {params: {id: string}}) {
 
     //validate the request body if invalid return 400
     const body = await request.json();
@@ -25,20 +25,35 @@ export async function PUT(request: NextRequest, {params}: {params: {id: number}}
     if(!validation.success)
         return NextResponse.json(validation.error.errors, {status: 400});
 
+    const user = await prisma.user.findUnique({
+        where: {id: parseInt(params.id)}
+    })
+
     //Fetch the user with the given id
-    if(params.id > 10)
+    if(!user)
         return NextResponse.json({error: 'User does not exist'}, {status: 404});
 
-    return NextResponse.json({id: 1, name: body.name});
+    const updatedUser = await prisma.user.update({
+        where: {id: user.id},
+        data: {
+            name: body.name,
+            email: body.email
+        }
+    });
+
+    return NextResponse.json(updatedUser);
 }
 
-export async function DELETE(request: NextRequest, {params}: {params: {id: number}}){
-    const body = await request.json();
+export async function DELETE(request: NextRequest, {params}: {params: {id: string}}){
 
-    if(params.id > 10)
+    const user = await prisma.user.delete({
+        where: {id: parseInt(params.id)}
+    })
+
+    if(!user)
         return NextResponse.json({error: 'User not found'}, {status: 404});
 
-    return NextResponse.json({});
+    return NextResponse.json({message: "user deleted successfully"});
 
 }
 
